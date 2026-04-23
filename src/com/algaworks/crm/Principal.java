fd36249aaac1c1555a1663b5045ab615fa29fc61
Uislan Cruz <uislansk8@hotmail.com>
2026-04-23 15:41:25 -0300

    Usando o ArrayList de forma pura, assim pode add qualquer objeto, porem dará erro devido a logica de faturamento

---
+++ src/com/algaworks/crm/Principal.java
@@ -0,0 +1,21 @@
+package com.algaworks.crm;
+
+import java.util.ArrayList;
+
+public class Principal {
+    static void main(String[] args) {
+        ArrayList clientes = new ArrayList();
+        clientes.add(new Cliente("Supermercado Pegue e leve", 5000_000));
+        clientes.add(new Cliente("Posto de gasolina", 800_000));
+
+        double totalFaturamento = 0d;
+
+        for (Object objeto : clientes) {
+            Cliente cliente = (Cliente) objeto;
+
+            totalFaturamento += cliente.getFaturamentoMensal();
+        }
+
+        System.out.println(totalFaturamento);
+    }
+}
