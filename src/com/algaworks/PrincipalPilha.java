76dd7465bba569987ac7c8df11e258ce7fd180f0
Uislan Cruz <uislansk8@hotmail.com>
2026-04-27 23:13:27 -0300

    Driando interfaces genéricas

--- src/com/algaworks/PrincipalPilha.java
+++ src/com/algaworks/PrincipalPilha.java
@@ -1,4 +1,22 @@
 package com.algaworks;
 
+import com.algaworks.estruturadedados.Pilha;
+import com.algaworks.loja.Produto;
+
 public class PrincipalPilha {
+    static void main(String[] args) {
+        Pilha <Produto> pilha = new Pilha<>();
+
+        pilha.colocar(new Produto("Arroz"));
+        pilha.colocar(new Produto("Feijão"));
+
+
+        Produto produto = pilha.retirar();
+        System.out.println(produto.getDescricao());
+
+        produto =  pilha.retirar();
+        System.out.println(produto.getDescricao());
+
+
+    }
 }
